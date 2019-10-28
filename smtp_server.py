8116c53a76132449342df7aa0118db07881c9879
IRemsha <40166328+IRemsha@users.noreply.github.com>
2019-10-28 23:44:34 +0500

    localhost and 25 on default

--- smtp_server.py
+++ smtp_server.py
@@ -133,7 +133,7 @@ class SMTPServer:
 
     def __init__(self, port=25, listeners=5):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind(('0.0.0.0', port))
+        self.socket.bind(('localhost', port))
         self.socket.listen(listeners)
 
     def socket_accept(self):
